7c9ddf8e050a4ee66499271b3980d5a4dfce69c2
Sarah Mckee <sarahmckee714@gmail.com>
2024-12-02 12:07:26 -0600

    Added To-Do list to main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Assign DOM elements to variables
+// Pull Traits, Tags, and Powers data from JSON
+  // Traits and Tags need "name" and "description"
+  // Powers need "name", "set", "cost", "pg number", "duration", "action", "summary"
+// Functions for updating dependent elements
+// Functions for adding, rendering, and deleting Traits, Tags, and Powers
+// Event listeners for the buttons
+// Localstorage functionality
