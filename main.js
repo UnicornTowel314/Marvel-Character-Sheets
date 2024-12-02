8c8bf5b4cef3018732192cc26bfa8fcbacb1086a
Sarah Mckee <sarahmckee714@gmail.com>
2024-12-02 14:52:20 -0600

    Added DOM variables to JS and repositioned buttons in HTML

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Assign DOM elements to variables
 // Pull Occupations, Traits, Tags, and Powers data from JSON
   // Occupations need "name", "description", and "traits"
   // Traits and Tags need "name" and "description"
@@ -7,3 +6,77 @@
 // Functions for adding, rendering, and deleting Traits, Tags, and Powers
 // Event listeners for the buttons
 // Localstorage functionality
+
+// IDENTITY VARIABLES
+const originInput = document.getElementById("origin");
+const originDesc = document.getElementById("origin-description");
+const originTags = document.getElementById("origin-tags");
+const originSugOccu = document.getElementById("origin-suggested-occupation");
+const occupInput = document.getElementById("occupation");
+const occuDesc = document.getElementById("occupation-description");
+const occuTraits = document.getElementById("occupation-traits");
+
+// PHYSICAL TRAITS VARIABLES
+const initMod = document.getElementById("init-mod");
+const currHealth = document.getElementById("curr-health");
+const maxHealth = document.getElementById("max-health");
+const currFocus = document.getElementById("curr-focus");
+const maxFocus = document.getElementById("max-focus");
+const speed = document.getElementById("speed");
+const run = document.getElementById("run");
+const climb = document.getElementById("climb");
+const swim = document.getElementById("swim");
+
+// TRAITS
+const traitInput = document.getElementById("trait-select");
+const traitDelInput = document.getElementById("traits-delete");
+
+// TAGS
+const tagInput = document.getElementById("tag-select");
+const tagDelInput = document.getElementById("tags-delete");
+
+// ABILITIES
+const meleeAb = document.getElementById("melee");
+const meleeDef = document.getElementById("melee-def");
+const meleeNC = document.getElementById("melee-non-com");
+const agilityAb = document.getElementById("agility");
+const agilityDef = document.getElementById("agility-def");
+const agilityNC = document.getElementById("agility-non-com");
+const resilienceAb = document.getElementById("resilience");
+const resilienceDef = document.getElementById("resilience-def");
+const resilienceNC = document.getElementById("resilience-non-com");
+const vigilanceAb = document.getElementById("vigilance");
+const vigilanceDef = document.getElementById("vigilance-def");
+const vigilanceNC = document.getElementById("vigilance-non-com");
+const egoAb = document.getElementById("ego");
+const egoDef = document.getElementById("ego-def");
+const egoNC = document.getElementById("ego-non-com");
+const logicAb = document.getElementById("logic");
+const logicDef = document.getElementById("logic-def");
+const logicNC = document.getElementById("logic-non-com");
+
+// DAMAGE
+const meleeDmg = document.getElementById("melee-dmg");
+const meleeMod = document.getElementById("melee-modifier");
+const agilityDmg = document.getElementById("agility-dmg");
+const agilityMod = document.getElementById("agility-modifier");
+const egoDmg = document.getElementById("ego-dmg");
+const egoMod = document.getElementById("ego-modifier");
+const logicDmg = document.getElementById("logic-dmg");
+const logicMod = document.getElementById("logic-modifier");
+
+// POWERS
+const powersInput = document.getElementById("power-select");
+const powersDelInput = document.getElementById("powers-delete");
+
+// BUTTONS
+const healthInc = document.getElementById("health-inc");
+const healthDec = document.getElementById("health-dec");
+const focusInc = document.getElementById("focus-inc");
+const focusDec = document.getElementById("focus-dec");
+const addTrait = document.getElementById("add-trait");
+const delTrait = document.getElementById("delete-trait");
+const addTag = document.getElementById("add-tag");
+const delTag = document.getElementById("delete-tag");
+const addPower = document.getElementById("add-power");
+const delPower = document.getElementById("delete-power");
